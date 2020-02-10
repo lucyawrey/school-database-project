640c15314020a97919bfef4ac6f0ab5cd17e38f5
Lucy Awrey <35198881+lucyawrey@users.noreply.github.com>
2020-02-10 14:40:30 -0500

    Database updates

--- edu/lucyawrey/student_database/Database.java
+++ edu/lucyawrey/student_database/Database.java
@@ -11,6 +11,7 @@ public class DataBase {
   private OrderedArray firstNameIndex;
   private OrderedArray lastNameIndex;
   private Stack deleteStack;
+  private Scanner scanner;
 
   public DataBase() {
     this(100);
@@ -23,6 +24,7 @@ public class DataBase {
     firstNameIndex = new OrderedArray(size);
     lastNameIndex = new OrderedArray(size);
     deleteStack = new Stack(size);
+    scanner = new Scanner(System.in);
 
     StudentRecord[] initialData = getRecordsFromFile("data.txt", size);
     if (initialData != null) {
@@ -36,9 +38,9 @@ public class DataBase {
     }
   }
 
-  public void insert(String id, String firstName, String lastName) {
+  public boolean insert(String id, String firstName, String lastName) {
     if (idIndex.containsKey(id)) {
-      return;
+      return false;
     }
 
     int location;
@@ -52,55 +54,93 @@ public class DataBase {
     idIndex.insert(new IndexRecord(id, location));
     firstNameIndex.insert(new IndexRecord(firstName, location));
     lastNameIndex.insert(new IndexRecord(lastName, location));
+    return true;
   }
 
   public StudentRecord search(String id) {
     int location = idIndex.getValue(id);
-    return databaseArray[location];
+    if (location != -1) {
+      return databaseArray[location];
+    } else {
+      return null;
+    }
   }
 
-  public void delete(String id) {
+  public boolean delete(String id) {
     int location = idIndex.getValue(id);
-    StudentRecord record = databaseArray[location];
-    idIndex.delete(id);
-    firstNameIndex.delete(record.firstName);
-    lastNameIndex.delete(record.lastName);
-    deleteStack.push(location);
+    if (location != -1) {
+      StudentRecord record = databaseArray[location];
+      idIndex.delete(id);
+      firstNameIndex.delete(record.firstName);
+      lastNameIndex.delete(record.lastName);
+      deleteStack.push(location);
+      return true;
+    } else {
+      return false;
+    }
   }
 
   public void addIt() {
-
+    System.out.println("Enter student record in the format '{id} {firstName} {lastName}:'");
+    String line = scanner.nextLine();
+    String[] tokens = line.split(" ");
+    boolean success = insert(tokens[0], tokens[1], tokens[2]);
+    if (success) {
+      System.out.println("Added student record");
+    } else {
+      System.out.println("ID already in use");
+    }
   }
 
   public void deleteIt() {
-
+    System.out.println("Enter ID of record to delete:");
+    String token = scanner.next();
+    boolean success = delete(token);
+    if (success) {
+      System.out.println("Deleted");
+    } else {
+      System.out.println("ID not found");
+    }
   }
 
   public void findIt() {
-
+    System.out.println("Enter ID of record to look up:");
+    String token = scanner.next();
+    StudentRecord record = search(token);
+    if (record != null) {
+      System.out.println("Retrieved record:" + record.id + ": " + record.lastName + ", " + record.firstName);
+    } else {
+      System.out.println("ID not found");
+    }
   }
 
   public void ListByIDAscending() {
+    System.out.println("Listing entire database in ascending order by ID...");
     listAscending(idIndex);
   }
 
   public void ListByFirstAscending() {
+    System.out.println("Listing entire database in ascending order by first name...");
     listAscending(firstNameIndex);
   }
 
   public void ListByLastAscending() {
+    System.out.println("Listing entire database in ascending order by last name...");
     listAscending(lastNameIndex);
   }
 
   public void ListByIDDescending() {
+    System.out.println("Listing entire database in descening order by ID...");
     listDescending(idIndex);
   }
 
   public void ListByFirstDescending() {
+    System.out.println("Listing entire database in descening order by first name...");
     listDescending(idIndex);
   }
 
   public void ListByLastDescending() {
+    System.out.println("Listing entire database in descening order by last name...");
     listDescending(idIndex);
   }
 
@@ -119,7 +159,7 @@ public class DataBase {
       scanner.close();
       return records;
     } catch(Exception e) {
-      System.out.println("File not found, skipping initial data.");
+      System.out.println("File not found, skipping initial data!");
       return null;
     }
   }
